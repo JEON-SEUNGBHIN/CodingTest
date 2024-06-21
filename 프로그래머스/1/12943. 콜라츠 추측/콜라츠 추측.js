1ce74298443e9a8240a4041f16ea8f294941f49d
JEON SEUNGBHIN <104238055+JEON-SEUNGBHIN@users.noreply.github.com>
2024-06-22 01:32:44 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.11 ms, Memory: 33.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -0,0 +1,11 @@
+function solution(num) {
+    var answer = 0;
+    
+    for(let i = 0; i < 500; i++) {
+        if (num === 1) {
+            return answer = i;
+        }
+        num = num % 2 === 0 ? num / 2 : num * 3 + 1;
+    }
+    return -1;
+}
