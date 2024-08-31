f0330a8d14eceba230cb4a5b35c6f35616c59124
JEON SEUNGBHIN <104238055+JEON-SEUNGBHIN@users.noreply.github.com>
2024-08-31 23:04:34 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
@@ -0,0 +1,8 @@
+function solution(s) {
+    const nums = s.split(' ').map(Number);
+    
+    const min = Math.min(...nums);
+    const max = Math.max(...nums);
+    
+    return `${min} ${max}`;
+}
